30691397cf922c2c135fd0ce27baadc6110d7fb9
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-29 23:32:20 +0600

    4th commit

--- M.js
+++ M.js
@@ -3,4 +3,18 @@ data= 'liam';
 data= 3.04;
 // data = true
 data ={name:'liam',cgpa:3.04}
+data = [2,3,5];
 console.log(typeof data)
+
+
+// js is a dynamic type programming language
+// Uniqe jinishgulo k bole premative type data
+// js unique data type
+const price=50;
+const name = 'liam';
+const ispoor =false;
+
+
+// Non premitive
+const bioData = {name:"liam", cgpa:3.04, education:'engineer'}
+const numbers =[1,2,3,4,5,6]
