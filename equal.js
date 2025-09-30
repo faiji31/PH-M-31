88f67a8067c7416650574a3708afd2c8ae4bf3e9
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-09-30 09:34:09 +0600

    8th commit

---
+++ equal.js
@@ -0,0 +1,16 @@
+// (==)  type coersion vitore vitore type convert kore
+// (===) condtion check kore jate bujha jai duita equa er man soman kina'
+
+// double equal
+
+console.log(2=='2');
+
+console.log(2==2);
+
+console.log(2=='12');
+
+// triple equal
+
+console.log(2===2);
+
+console.log(31===30);
